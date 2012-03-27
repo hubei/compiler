fc2ecf831687f2aec9465c6a0670450244a8c3e3
DerBasti <seb.nickel@googlemail.com>
2012-03-27 14:03:15 +0200

    Symboltable update

--- src/symboltable.h
+++ src/symboltable.h
@@ -53,6 +53,6 @@ void insertVar(string, int, string); //ID, type(0 int, 1 void), scopeID
 void insertArray(string, int, string, int); //ID, type(0 int, 1 void), scopeID, size
 void insertFunc(string, int, int[]); //ID, type(0 int, 1 void), parameter typen
 
-bool exist(string, string); //ID, scopeID
+//bool exist(string, string); //ID, scopeID
 
 #endif
